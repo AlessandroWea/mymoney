f9c554adeb6d2a9d1d613b3c9b2e31ca2a88caf1
AlessandroWea <atribun14@gmail.com>
2023-08-24 16:59:17 +1000

    add function to get data from GET var, function to get data for checkboxes from POST

--- src/functions.php
+++ src/functions.php
@@ -12,6 +12,12 @@ function post($key, $default = '')
     return isset($_POST[$key]) ? $_POST[$key] : $default;
 }
 
+function get($key, $default = '')
+{
+    return isset($_GET[$key]) ? $_GET[$key] : $default;
+
+}
+
 function path($path)
 {
     return '/' . $path;
@@ -25,4 +31,15 @@ function selected($key, $val)
     }
 
     return '';
+}
+
+function checked($key, $val, $default = null)
+{
+    if(isset($_POST[$key]) && $_POST[$key] == $val)
+        return ' checked ';
+    else if($default !== null && $val == $default)
+        return ' checked ';
+
+    return '';
+
 }

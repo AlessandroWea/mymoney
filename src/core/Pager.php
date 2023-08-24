d0bc93f82888327976b91b8bbf369a0d639f1c37
AlessandroWea <atribun14@gmail.com>
2023-08-24 17:02:34 +1000

    Pager class for pagination

--- src/core/Pager.php
+++ src/core/Pager.php
@@ -15,18 +15,19 @@ class Pager
         $this->offset = ($this->page - 1) * $this->limit;
     }
 
-    public function display()
+    public function display(array $extra = [])
     {
+        $search = $extra['search'] ?? '';
         $str = '<nav aria-label="Page navigation example">
         <ul class="pagination">';
         if($this->page == 1)
-            $str .= '<li class="page-item"><a class="page-link" href="?page=1">Previous</a></li>';
+            $str .= '<li class="page-item"><a class="page-link" href="?page=1&search=' . $search .'">Previous</a></li>';
         else
-            $str .= '<li class="page-item"><a class="page-link" href="?page=' . $this->page-1 . '">Previous</a></li>';
+            $str .= '<li class="page-item"><a class="page-link" href="?page=' . $this->page-1 . '&search=' . $search .'">Previous</a></li>';
         $str .= '<li class="page-item active"><a class="page-link" href="?page=' . $this->page . '">' . $this->page .'</a></li>';
-        $str .= '<li class="page-item"><a class="page-link" href="?page=' . $this->page+1 . '">' . $this->page+1 .'</a></li>';
-        $str .= '<li class="page-item"><a class="page-link" href="?page=' . $this->page+2 . '">' . $this->page+2 .'</a></li>';
-        $str .= '<li class="page-item"><a class="page-link" href="?page=' . $this->page+1 .'">Next</a></li>';
+        $str .= '<li class="page-item"><a class="page-link" href="?page=' . $this->page+1 . '&search=' . $search .'">' . $this->page+1 .'</a></li>';
+        $str .= '<li class="page-item"><a class="page-link" href="?page=' . $this->page+2 . '&search=' . $search .'">' . $this->page+2 .'</a></li>';
+        $str .= '<li class="page-item"><a class="page-link" href="?page=' . $this->page+1 .'&search=' . $search . '">Next</a></li>';
        $str .= ' </ul></nav>';
 
        echo $str;

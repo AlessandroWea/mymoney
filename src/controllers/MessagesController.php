284833b84d54f449958dcf4a16fb0bc157749411
AlessandroWea <atribun14@gmail.com>
2024-02-06 11:46:07 +1000

    UPDATE: new messages are marked as read when viewed

--- src/controllers/MessagesController.php
+++ src/controllers/MessagesController.php
@@ -43,10 +43,12 @@ class MessagesController extends Controller
         $convers = $conversationModel->first(['id'=>$id]);
         if($convers['id_user1'] == $_SESSION['USER']['id'])
         {
+            $partner_id = $convers['id_user2'];
             $user = $userModel->first(['id'=>$convers['id_user2']]);
         }
         else
         {
+            $partner_id = $convers['id_user1'];
             $user = $userModel->first(['id'=>$convers['id_user1']]);
         }
 
@@ -55,6 +57,14 @@ class MessagesController extends Controller
             'id_conversation' => $id,
         ]);
         
+        // if there are new messages, change their property 'is_read' to 1 (so, it is read)
+        $messageModel->updateWhere([
+            'id_conversation' => $id,
+            'id_user' => $partner_id,
+        ], [
+            'is_read' => 1,
+        ]);
+
         $this->view('messages/single', [
             'messages' => $messages,
             'user' => $user,
